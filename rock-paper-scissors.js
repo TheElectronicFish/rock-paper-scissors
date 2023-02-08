5594060e0b017b103acba3e72edc65a98e41a5d4
Gage Sturgeon <gsjets33@gmail.com>
2023-02-08 13:10:07 -0800

    Add player choice function

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -2,9 +2,17 @@
 // create a variable to hold the computer's choice
 // create a function that chooses randomly from rock, paper, and scissors.
 
+
+function getPlayerChoice() {
+    let choice = prompt("Enter your answer");
+    choice = choice.toLowerCase();
+    return choice;
+}
+
 function getComputerChoice() {
     const choice = ["rock", "paper", "scissors"];
     return choice[Math.floor(Math.random() * choice.length)];
 }
 
+console.log(getPlayerChoice());
 console.log(getComputerChoice());
