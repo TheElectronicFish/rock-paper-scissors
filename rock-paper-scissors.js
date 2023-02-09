e0e3c24be9d8d5cc0db4f50e989f0ee167c25c69
Gage Sturgeon <gsjets33@gmail.com>
2023-02-08 19:38:18 -0800

    Complete game function

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,6 +1,26 @@
 
 // create a variable to hold the computer's choice
-// create a function that chooses randomly from rock, paper, and scissors.
+// create a function that chooses randomly from rock, paper, and scissors for computer
+// assign random choice to computer choice variable
+
+// create a variable to hold the player's choice
+// create a function to prompt the player for their choice
+// assign the player's choice to their variable
+
+// create a function that plays a game of RPS which picks a choice for the user and computer
+// create a variable for points
+// compare the computer and player choice to determine who wins.
+// console display both the player's and computer"s choice
+// console display the results of the game
+// set points variable according to results and return
+
+// create a function to play five games of rps
+// initialize a variable for the total points
+// create a sub-function for loop to increment a variable over the five games
+// add up the points as the games complete
+
+// create a function to compare the total points to determine if it was a win, loss, or tie for the player
+
 
 
 function getPlayerChoice() {
@@ -14,34 +34,78 @@ function getComputerChoice() {
     return choice[Math.floor(Math.random() * choice.length)];
 }
 
-const playerSelection = getPlayerChoice();
-const computerSelection = getComputerChoice();
-
-console.log(playerSelection);
-console.log(computerSelection);
-
 function playGame() {
+    const playerSelection = getPlayerChoice();
+    const computerSelection = getComputerChoice();
+
+    console.log(playerSelection);
+    console.log(computerSelection);
+
     if (playerSelection == "rock" && computerSelection == "rock") {
         console.log("Tie. Rock equals rock.");
+        score = 0;
+        return score;
     }else if (playerSelection == "rock" && computerSelection == "scissors") {
         console.log("You win! Rock beats scissors.");
+        score = 1;
+        return score;
     }else if (playerSelection == "rock" && computerSelection == "paper") {
         console.log("You lose. Paper beats rock.");
+        score = -1;
+        return score;
     }else if (playerSelection == "paper" && computerSelection == "rock") {
         console.log("You Win! Paper beats rock.");
+        score = 1;
+        return score;
     }else if (playerSelection == "paper" && computerSelection == "scissors") {
         console.log("You lose. Scissors beats paper.");
+        score = -1;
+        return score;
     }else if (playerSelection == "paper" && computerSelection == "paper") {
         console.log("Tie. Paper equals paper.");
+        score = 0;
+        return score;
     }else if (playerSelection == "scissors" && computerSelection == "rock") {
         console.log("You lose. Rock beats scissors.");
+        score = -1;
+        return score;
     }else if (playerSelection == "scissors" && computerSelection == "scissors") {
         console.log("Tie. Scissors equals scissors.");
+        score = 0;
+        return score;
     }else if (playerSelection == "scissors" && computerSelection == "paper") {
         console.log("You win! Scissors beats paper.");
+        score = 1;
+        return score;
     }else {
         console.log("Please enter rock, paper, or scissors.")};
+        score = 0;
+        return score;
 
 }
 
-playGame();
+function game() {
+    
+    let result = 0;
+
+    function playRound() {
+        for (let i = 0; i < 5; i++) {
+            result = +result + playGame();
+        }
+    }
+
+    function winnerLooser() {
+    if (result > 0) {
+        console.log(`WINNER`)
+    }else if (result < 0) {
+        console.log(`LOSER`)
+    }else {
+        console.log(`TIE`)
+    }
+    }
+
+    playRound();
+    winnerLooser();
+}
+
+game();
